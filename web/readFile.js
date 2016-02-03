d7342387e30371bd1a469ff9e614a2d640e48a78
Lance Laughlin <llaughlin@sapient.com>
2016-02-03 11:40:18 -0500

    Finally

--- web/readFile.js
+++ web/readFile.js
@@ -1,17 +1,42 @@
 document.getElementById('files').addEventListener('change', handleFileSelect, false);
 
-var margin = {top: 100, right: 100, bottom: 100, left: 100},
-width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
+var margin = {top: 50, right: 50, bottom: 50, left: 50},
+width = Math.min(800, window.innerWidth - 10) - margin.left - margin.right,
 height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
 					
-var dayArray = [];
 var sets;
 var setLow = 0;
-var setHigh = 3600;
+var setHigh = 3599;
 var fullData = [
 	 //  [
 		// {value:0.0},
 		// {value:93.0},
+		// {value:108.0},
+		// {value:180.0},
+		// {value:78.0},
+		// {value:147.0},
+		// {value:78.0},
+		// {value:135.0},			
+		// {value:138.0},			
+		// {value:75.0},			
+		// {value:105.0},			
+		// {value:222.0},			
+		// {value:87.0},			
+		// {value:84.0},			
+		// {value:222.0},			
+		// {value:120.0},			
+		// {value:105.0},			
+		// {value:222.0},			
+		// {value:87.0},			
+		// {value:84.0},		
+		// {value:222.0},		
+		// {value:105.0},		
+		// {value:72.0},		
+		// {value:84.0},		
+	 //  ],
+	 //   [
+		// {value:0.0},
+		// {value:100.0},
 		// {value:108.0},
 		// {value:180.0},
 		// {value:78.0},
@@ -41,11 +66,11 @@ var fullData = [
 ////////////////////////////////////////////////////////////// 
 
 var color = d3.scale.ordinal()
-	.range(["#7cb5ec","#7cb5ec","#7cb5ec"]);
+	.range(["#00aeef"]);
 
 var radarChartOptions = {
-	w: width,
-	h: height,
+	w: 800,
+	h: 800,
 	margin: margin,
 	maxValue: 327,
 	levels: 6,
@@ -58,7 +83,6 @@ var radarChartOptions = {
 function handleFileSelect(evt) {
 	var files = evt.target.files; // FileList object
 
-	// Loop through the FileList and render image files as thumbnails.
 	for (var i = 0, f; f = files[i]; i++) {
 
 	  var reader = new FileReader();
@@ -70,36 +94,46 @@ function handleFileSelect(evt) {
 	    };
 	  })(f);
 
-	  // Read in the image file as a data URL.
+	  // Read in the file as text
 	  reader.readAsText(f);
 	}
 }
 
 function populateData(data){
 	dataArray = data.split(",");
-	sets = dataArray.length / setHigh;
-	for(var i = 0; i <= sets; i++){
-		for(var j = setLow; j <= 3600; j++){
+
+	sets = dataArray.length / (setHigh + 1);
+	for(var i = 0; i < sets; i++){
+
+		var dayArray = [];
+
+		console.log("current setLow: " + setLow);
+		console.log("current setHigh: " + setHigh);
+		for(var j = setLow; j < setHigh; j++){
+			dataArray[j] = $.trim(dataArray[j]);
 			dayArray.push({
 				value: parseInt(dataArray[j])
 			});
 		}
-		
-		console.log("setLow: " + setLow);
-		console.log("setHigh: " + setHigh);
-		
-		setLow = setHigh;
-		if(setHigh * 2 > dataArray.length){
-			setHigh = dataArray.length;
-		}else{
-			setHigh = setHigh * 2;
-		}
-		//console.log("setHigh: " + setHigh);
 
 		fullData.push(dayArray);
+
+		console.log(fullData);
+
+		
+		setLow = setHigh + 1;
+		console.log(setLow * 2);
+		console.log(dataArray.length);
+		if(setLow * 2 >= dataArray.length){
+		 	setHigh = dataArray.length;
+		 	console.log("should be 7200:" + setHigh);
+		}else{
+			setHigh = setLow * 2;
+			console.log("should be 7198:" + setHigh);
+		}
+		
 	}
 
-	console.log(fullData);
 
 	RadarChart(".radarChart", fullData, radarChartOptions);
 
